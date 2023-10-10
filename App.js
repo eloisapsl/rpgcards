83ee709d84084d22dd9efd3f62ac3ed25e11fc98
eloisapsl <111752882+eloisapsl@users.noreply.github.com>
2023-10-09 23:01:14 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import card1 from './cards/card1';
+import CardUm from './cards/CardUm';
+import CardDois from './cards/CardDois';
+import CardTres from './cards/CardTres';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Card1" component={card1}/>
-
+        <Stack.Screen name="cardum" component={CardUm}/>
+        <Stack.Screen name="carddois" component={CardDois}/>
+        <Stack.Screen name="cardtres" component={CardTres}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ cards/CardDois/index.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { View, Text, Image, TouchableOpacity} from 'react-native';
+import { Link } from '@react-navigation/native';
+import styles from './styles'
+
+
+export default function CardDois(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.card}> 
+                <View style={styles.titulo1}>
+                    <Text style={styles.texto1}> Card 2</Text>
+                </View>
+
+                <Image source={require('../../assets/mestremagos.png')} style={styles.imagem}></Image>
+                <Text style={styles.texto2}> ⚜ Mestre dos Magos ⚜ </Text>
+                <Text style={styles.descricao}>
+                    Esta carta permite que o jogador se ausente da rodada enquanto está sendo atacado por forças inimigas. Kabum!
+                </Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.txtbutton}>Trocar carta</Text>
+                </TouchableOpacity>
+                <Link to={{ screen: 'CardTres' }} style={styles.link}>
+      Próxima carta →
+    </Link>
+            </View>
+        </View>
+    )
+}
